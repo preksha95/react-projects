a8795fc18cfcfeb65f103840619efcf0cadff82a
preksha95 <prekshaan95@gmail.com>
2019-12-20 21:41:04 +0530

    changes

--- my_react_app/src/components/Biodata.js
+++ my_react_app/src/components/Biodata.js
@@ -3,24 +3,34 @@ import React,{Component} from 'react';
 class Biodata extends Component{
 
   constructor(props){
-    var name,place
     super(props);
-    props.info.forEach(element => {
-      this.name=element.name
-      this.place=element.place
-});
+    this.state={
+      name: props.name,
+      place: props.place,
+      phone_no: ""
+    }
 }
 
-contactInfo(){
+moreContactInfo(){
     this.setState({
-      place: 'Mumbai'
+      phone_no: "Working in Smarsh India Pvt Ltd"
     })
   }
+lessContactInfo(){
+  this.setState({
+    phone_no: ""
+  })
+}
 render(){
     return (
       <div>
-      <h1>I am {this.name} Living in {this.place}</h1>
-      <button onClick={()=>this.contactInfo()}>Click Here to Contact Me </button>
+      <h1>I am {this.state.name} Living in {this.state.place}</h1>
+      <p>{this.state.phone_no}</p>
+      <span>
+        <button onClick={()=>this.moreContactInfo()}>Click Here to Know More </button>
+        <button onClick={()=>this.lessContactInfo()}>Click Here to Know Less </button>
+      </span>
+
 
       </div>
     )
